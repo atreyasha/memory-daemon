9bb98c8009cf136d160323fad803e06ccdc86bca
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2020-04-22 13:19:03 +0200

    *shankar* minor clean-up plus bugfix for foreign used declaration

--- mem_daemon_mail.py
+++ mem_daemon_mail.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# get dependencies
 import re
 import smtplib
 import datetime
 import argparse
 from email.utils import formatdate
 from email.mime.text import MIMEText
-
-#########################
-# define email function
-#########################
 
 def getCurrentTime():
     return datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
@@ -21,19 +16,27 @@ def sendMail(receiver,sender,password,text,smtp,port,
     # define mail variables
     subject_1 = "[Process Termination Notification] "
     subject_0 = "[RAM Threshold Notification] "
-    pre_text_1 = 'Dear User,\n\n`{}` was terminated on {} as the total used RAM threshold of {}% was exceeded.\n\nBelow is a footprint of the process before it was terminated:\n\n'
-    pre_text_0 = 'Dear User,\n\nThe total used RAM threshold of {}% was exceeded by a process from another user `{}` on {}.\n\nBelow is a footprint of the process, for your information:\n\n'
-    end = "\n\nBest,\n\nYour Memory Daemon"
+    pre_text_1 = ('Dear User,\n\n`{}` was terminated on {} as the total used '
+                  'RAM threshold of {}% was exceeded.\n\nBelow is a footprint '
+                  'of the process before it was terminated:\n\n')
+    pre_text_0 = ('Dear User,\n\nThe total used RAM threshold of {}% '
+                  'was exceeded by a process from another user `{}`'
+                  ' on {}.\n\nBelow is a footprint of the process,'
+                  ' for your information:\n\n')
+    end = "\n\nYour Memory Daemon"
     # get command information from (assumed) last column
     command = text.split()[(col_log*2)-1]
+    user = text.split()[(col_log+2)-1]
     if selfie == 1:
         subject = subject_1 + command
         pre_text = pre_text_1.format(command,getCurrentTime(),threshold)
     elif selfie == 0:
         subject = subject_0 + command
-        pre_text = pre_text_0.format(threshold,command,getCurrentTime())
-    text = pre_text + "```\n" + re.sub("<space>","\n",re.sub(r"\s+"," ",
-                                       re.sub(r"\n","<space>",text)))+ "\n```"+ end
+        pre_text = pre_text_0.format(threshold,user,getCurrentTime())
+    text = (pre_text + "```\n"
+            + re.sub("<space>","\n",re.sub(r"\s+"," ",
+                                           re.sub(r"\n","<space>",text)))
+            + "\n```" + end)
     msg = MIMEText(text)
     msg['Subject'] = subject
     msg['From'] = sender
@@ -51,10 +54,6 @@ def sendMail(receiver,sender,password,text,smtp,port,
         s.sendmail(sender,rec,msg.as_string())
     s.quit()
 
-#########################
-# main command call
-#########################
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     requiredNamed = parser.add_argument_group('required named arguments')
@@ -65,13 +64,17 @@ if __name__ == "__main__":
     requiredNamed.add_argument('--password',
                                help='email sender password', required=True)
     requiredNamed.add_argument('--smtp', help='smtp address', required=True)
-    requiredNamed.add_argument('--port', help='smtp port', type=int, required=True)
-    requiredNamed.add_argument('--text', help='raw message to send', required=True)
-    requiredNamed.add_argument('--threshold', help='RAM threshold', required=True)
+    requiredNamed.add_argument('--port', help='smtp port', type=int,
+                               required=True)
+    requiredNamed.add_argument('--text', help='raw message to send',
+                               required=True)
+    requiredNamed.add_argument('--threshold', help='RAM threshold',
+                               required=True)
     requiredNamed.add_argument('--columns-log', help='columns in log file',
                                type=int, required=True)
     requiredNamed.add_argument('--selfie', type=int,
-                               help='whether own process or from someone else', required=True)
+                               help='whether own process or from someone else',
+                               required=True)
     args = parser.parse_args()
     sendMail(args.receiver,args.sender,args.password,args.text,
              args.smtp,args.port,args.threshold,args.columns_log,
