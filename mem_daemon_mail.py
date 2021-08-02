c1147899ee24d74cded8a59c5d22225309c87faf
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2021-08-02 14:07:45 +0200

    Yapf-linting of python source code

--- mem_daemon_mail.py
+++ mem_daemon_mail.py
@@ -8,14 +8,16 @@ import argparse
 from email.utils import formatdate
 from email.mime.text import MIMEText
 
+
 def getCurrentTime():
     """
     Function that returns a date-time string for logging
     """
     return datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
 
-def sendMail(receiver,sender,password,text,smtp,port,
-             threshold,col_log,selfie):
+
+def sendMail(receiver, sender, password, text, smtp, port, threshold, col_log,
+             selfie):
     """
     Function to send out email based on output of mem_daemon
 
@@ -42,57 +44,64 @@ def sendMail(receiver,sender,password,text,smtp,port,
                   ' for your information:\n\n')
     end = "\n\nYour Memory Daemon"
     # get command information from (assumed) last column
-    command = text.split()[(col_log*2)-1]
-    user = text.split()[(col_log+2)-1]
+    command = text.split()[(col_log * 2) - 1]
+    user = text.split()[(col_log + 2) - 1]
     if selfie == 1:
         subject = subject_1 + command
-        pre_text = pre_text_1.format(command,getCurrentTime(),threshold)
+        pre_text = pre_text_1.format(command, getCurrentTime(), threshold)
     elif selfie == 0:
         subject = subject_0 + command
-        pre_text = pre_text_0.format(threshold,user,getCurrentTime())
-    text = (pre_text + "```\n"
-            + re.sub("<space>","\n",re.sub(r"\s+"," ",
-                                           re.sub(r"\n","<space>",text)))
-            + "\n```" + end)
+        pre_text = pre_text_0.format(threshold, user, getCurrentTime())
+    text = (pre_text + "```\n" + re.sub(
+        "<space>", "\n", re.sub(r"\s+", " ", re.sub(r"\n", "<space>", text))) +
+            "\n```" + end)
     msg = MIMEText(text)
     msg['Subject'] = subject
     msg['From'] = sender
     msg['To'] = receiver
     msg["Date"] = formatdate(localtime=True)
     # connect to SMTP server
-    s = smtplib.SMTP(smtp,port)
+    s = smtplib.SMTP(smtp, port)
     # start TLS secure connection
     s.starttls()
     # log into SMTP server
-    s.login(sender,password)
+    s.login(sender, password)
     # send email(s) and quit
-    receiver=re.split(r"\s*,\s*",receiver)
+    receiver = re.split(r"\s*,\s*", receiver)
     for rec in receiver:
-        s.sendmail(sender,rec,msg.as_string())
+        s.sendmail(sender, rec, msg.as_string())
     s.quit()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     requiredNamed = parser.add_argument_group('required named arguments')
     requiredNamed.add_argument('--receiver',
-                               help='email receipient(s)', required=True)
-    requiredNamed.add_argument('--sender',
-                               help='email sender', required=True)
+                               help='email receipient(s)',
+                               required=True)
+    requiredNamed.add_argument('--sender', help='email sender', required=True)
     requiredNamed.add_argument('--password',
-                               help='email sender password', required=True)
+                               help='email sender password',
+                               required=True)
     requiredNamed.add_argument('--smtp', help='smtp address', required=True)
-    requiredNamed.add_argument('--port', help='smtp port', type=int,
+    requiredNamed.add_argument('--port',
+                               help='smtp port',
+                               type=int,
                                required=True)
-    requiredNamed.add_argument('--text', help='raw message to send',
+    requiredNamed.add_argument('--text',
+                               help='raw message to send',
                                required=True)
-    requiredNamed.add_argument('--threshold', help='RAM threshold',
+    requiredNamed.add_argument('--threshold',
+                               help='RAM threshold',
                                required=True)
-    requiredNamed.add_argument('--columns-log', help='columns in log file',
-                               type=int, required=True)
-    requiredNamed.add_argument('--selfie', type=int,
+    requiredNamed.add_argument('--columns-log',
+                               help='columns in log file',
+                               type=int,
+                               required=True)
+    requiredNamed.add_argument('--selfie',
+                               type=int,
                                help='whether own process or from someone else',
                                required=True)
     args = parser.parse_args()
-    sendMail(args.receiver,args.sender,args.password,args.text,
-             args.smtp,args.port,args.threshold,args.columns_log,
-             args.selfie)
+    sendMail(args.receiver, args.sender, args.password, args.text, args.smtp,
+             args.port, args.threshold, args.columns_log, args.selfie)
